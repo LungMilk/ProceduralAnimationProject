ab16ac797aa2ad963e2eae0a3bc934b115770e88
Brainmush <joeplace913@gmail.com>
2025-11-03 14:55:59 -0500

    Body now restricts itself to a set height
    devoting too much time to setting height so moving on to another task

--- proceduralAnimationProject/Assets/Scripts/GetHeight.cs
+++ proceduralAnimationProject/Assets/Scripts/GetHeight.cs
@@ -8,33 +8,43 @@ public class GetHeight : MonoBehaviour
     public LayerMask terrainLayer;
     public Transform body;
     private float distanceToFloor;
-    public float height;
+    public float maxHeight;
     private Vector3 offsetPosition;
     private void Update()
     {
         //each frame I want to set the hips to some distance above the ground
-        SetPositionToOffset();
+        //SetPositionToOffset();
+        DistanceToFloor();
     }
-    float GetDistanceToFloor()
+    void DistanceToFloor()
     {
-        //shoot a ray downward
-        Ray ray = new Ray(body.position, Vector3.down);
+        //shoot a ray downward but move the origin up a bit as to prevent getting around being in the ground
+        Ray ray = new Ray(body.position + (Vector3.up * 0.1f), Vector3.down);
+        Debug.DrawRay(body.position + (Vector3.up * 0.1f),Vector3.down,Color.red);
         if (Physics.Raycast(ray, out RaycastHit hit, 10, terrainLayer.value))
         {
             //distance from point and the bodys position is the offset in which to maintain
             float distance = Vector3.Distance(hit.point, body.position);
             //how do we set our position to be this distance
             //take the hit point and increase it up to find our desired positio
-            distance = distanceToFloor;
+            //offsetPosition = hit.point * distance;
+            //distance = distanceToFloor;
+            ////I want this distance to the floor to be compared to the 
+            if (distance < maxHeight)
+            {
+                Debug.DrawLine(hit.point, body.position,Color.green);
+                body.position = hit.point + (body.up * maxHeight);
+                print(distance);
+            }
         }
-        return distanceToFloor;
     }
-    void SetPositionToOffset()
-    {
-        //Set the body position to a place above the floor by some units.
-        if (GetDistanceToFloor() > height)
-        {
-            body.position = body.position + (body.up * height);
-        }
-    }
+    //void SetPositionToOffset()
+    //{
+    //    //Set the body position to a place above the floor by some units.
+    //    if (GetDistanceToFloor() < height)
+    //    {
+    //        print("Too close to ground");
+    //        body.position = offsetPosition;
+    //    }
+    //}
 }
